f77cbe1b1c5a59fa08566a7b414feda61a69437f
seanh <seanh@savlonic>
2016-10-28 22:22:17 -0500

    adding pre-existing files, merging with local mirror

---
+++ physics.c
@@ -0,0 +1,16 @@
+#include <u.h>
+#include <libc.h>
+#include <stdio.h>
+
+
+/***
+Equations are derived from a variety of sources, good references include:
+• http://ipl.physics.harvard.edu/wp-uploads/2013/03/PS3_Error_Propagation_sp13.pdf
+
+***/
+
+
+/* propagation of error through addition */
+prop_add(a uncertainty, b uncertainty)
+{
+}
